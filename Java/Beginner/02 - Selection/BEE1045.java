0b286d8e04de493bde07fd055a3bc12cf08e37bd
arthurcsilva <acs.arthursilva@gmail.com>
2026-01-05 21:01:24 -0300

    Envio dos exercicios 1042...1045

---
+++ Java/Beginner/02 - Selection/BEE1045.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+
+public class BEE1045 {
+    public static void main(String[] args) {
+        double menor =0.0, meio=0.0, maior =0.0;
+
+        Scanner scanner = new Scanner(System.in);
+        double a = scanner.nextDouble();
+        double b = scanner.nextDouble();
+        double c = scanner.nextDouble();
+
+        if (a > b && a > c) {
+            maior = a;
+            if (b > c) {
+                meio = b;
+                menor = c;
+            } else {
+                meio = c;
+                menor = b;
+            }
+        } else if (b > a && b > c) {
+                    maior = b;
+                    if (a > c) {
+                        meio = a;
+                        menor = c;
+                    } else{
+                        meio = c;
+                        menor = a;
+                    }
+        } else {
+            maior = c;
+            if(a > b){
+                meio = a;
+                menor = b;
+            }else {
+                meio = b;
+                menor = a;
+            }
+        }
+
+        if (maior >= meio + menor) {  // Verifica se forma triângulo
+            System.out.println("NAO FORMA TRIANGULO");
+        } else {
+
+            // Classificação por ângulo
+            double maior2 = maior * maior;
+            double soma2 = (meio * meio) + (menor * menor);
+
+            if (maior2 == soma2) {
+                System.out.println("TRIANGULO RETANGULO");
+            } else if (maior2 > soma2) {
+                System.out.println("TRIANGULO OBTUSANGULO");
+            } else {
+                System.out.println("TRIANGULO ACUTANGULO");
+            }
+
+            // Classificação por lados
+            if (maior == meio && meio == menor) {
+                System.out.println("TRIANGULO EQUILATERO");
+            } else if (maior == meio || maior == menor || meio == menor) {
+                System.out.println("TRIANGULO ISOSCELES");
+            }
+        }
+
+        scanner.close();
+
+    }
+}
