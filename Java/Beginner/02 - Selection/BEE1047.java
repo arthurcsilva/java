8cf315f9e7ec6640a1b56cb5ef8ffcdbf8de4446
arthurcsilva <acs.arthursilva@gmail.com>
2026-01-10 09:43:37 -0300

    Adição Exerc. 1047 e 1051

---
+++ Java/Beginner/02 - Selection/BEE1047.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class BEE1047 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int hrIn = scanner.nextInt();
+        int minIn = scanner.nextInt();
+        int hrFin = scanner.nextInt();
+        int minFin = scanner.nextInt();
+
+        // conversao para minutos
+        int inicio = hrIn * 60 + minIn;
+        int fim = hrFin * 60 + minFin;
+
+        // Se terminou no mesmo horário ou antes, passou 24h
+        if (fim <= inicio) {
+            fim += 24 * 60;
+        }
+
+        int duracao = fim - inicio;
+
+        int hrDuracao = duracao / 60;
+        int minDuracao = duracao % 60;
+
+        System.out.println(
+                "O JOGO DUROU " + hrDuracao + " HORA(S) E " + minDuracao + " MINUTO(S)"
+        );
+
+        scanner.close();
+    }
+}
